73347bc527fef1aff142eda5dd220507de9306fd
Simon Haugen <smn.hgn@gmail.com>
2020-12-09 18:47:54 +0100

    added full cube and fixed perspective

--- src/cube/Cube.svelte
+++ src/cube/Cube.svelte
@@ -7,6 +7,7 @@
 
   const unsubscribe = settings.subscribe(({ size }) => {
     piecePositions = [
+      // Layer 1
       { x: 0, y: 0, z: 0 },
       { x: 0, y: `${size}px`, z: 0 },
       { x: 0, y: `${2 * size}px`, z: 0 },
@@ -16,31 +17,55 @@
       { x: `${2 * size}px`, y: 0, z: 0 },
       { x: `${2 * size}px`, y: `${size}px`, z: 0 },
       { x: `${2 * size}px`, y: `${2 * size}px`, z: 0 },
+      // Layer 2
+      { x: 0, y: 0, z: `${size}px` },
+      { x: 0, y: `${size}px`, z: `${size}px` },
+      { x: 0, y: `${2 * size}px`, z: `${size}px` },
+      { x: `${size}px`, y: 0, z: `${size}px` },
+      { x: `${size}px`, y: `${size}px`, z: `${size}px` },
+      { x: `${size}px`, y: `${2 * size}px`, z: `${size}px` },
+      { x: `${2 * size}px`, y: 0, z: `${size}px` },
+      { x: `${2 * size}px`, y: `${size}px`, z: `${size}px` },
+      { x: `${2 * size}px`, y: `${2 * size}px`, z: `${size}px` },
+      // Layer 3
+      { x: 0, y: 0, z: `${2 * size}px` },
+      { x: 0, y: `${size}px`, z: `${2 * size}px` },
+      { x: 0, y: `${2 * size}px`, z: `${2 * size}px` },
+      { x: `${size}px`, y: 0, z: `${2 * size}px` },
+      { x: `${size}px`, y: `${size}px`, z: `${2 * size}px` },
+      { x: `${size}px`, y: `${2 * size}px`, z: `${2 * size}px` },
+      { x: `${2 * size}px`, y: 0, z: `${2 * size}px` },
+      { x: `${2 * size}px`, y: `${size}px`, z: `${2 * size}px` },
+      { x: `${2 * size}px`, y: `${2 * size}px`, z: `${2 * size}px` },
     ];
   });
 
   onDestroy(() => unsubscribe());
 </script>
 
-<div
-  class="cube"
-  style="
-    width: {$settings.size * 3}px;
-    height: {$settings.size * 3}px;
-    transform: 
-      rotateX({$settings.rotateX}deg) 
-      rotateY({$settings.rotateY}deg);
-    transform-origin: 50% 50% {$settings.size}px;
-    
-  ">
-  {#each piecePositions as position}
-    <Piece {position} />
-  {/each}
+<div class="cube-container">
+  <div
+    class="cube"
+    style="
+      width: {$settings.size * 3}px;
+      height: {$settings.size * 3}px;
+      transform: 
+        rotateX({$settings.rotateX}deg) 
+        rotateY({$settings.rotateY}deg);
+      transform-origin: 50% 50% {$settings.size}px;
+      
+    ">
+    {#each piecePositions as position}
+      <Piece {position} />
+    {/each}
+  </div>
 </div>
 
 <style>
+  .cube-container {
+    perspective: 500px;
+  }
   .cube {
-    perspective: 500;
     transform-style: preserve-3d;
   }
 </style>
